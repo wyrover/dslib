e9e6d5cc6593537d2e21f25593b21790259be727
Anirudh Murali <anirumur@cisco.com>
2015-10-20 12:39:48 -0700

    Fix compilation in slist.c +34

--- slist.c
+++ slist.c
@@ -31,7 +31,7 @@ static slist_t* slist_create_node(int data)
 {
     slist_t* new_node;
     new_node = (slist_t*)malloc(sizeof(slist_t));
-    if(!new_nodel) {
+    if(!new_node) {
         return NULL;
     }
     new_node-> data = data;
